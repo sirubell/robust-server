11808ae35f46d4b6ba67e7a3201b00ef9f60a84a
NTPU Computer Center <NTPU Computer Center@B1F06-35>
2022-03-28 14:26:23 +0800

    加入專案檔案。

---
+++ server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static public void Main()
+    {
+        Int32 port = 12345;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        TcpListener server = new TcpListener(localAddr, port);
+        server.Start();
+        Console.WriteLine("Server Start!");
+
+        try
+        {
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                StartClientThread(client);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            server.Stop();
+        }
+    }
+
+    static void StartClientThread(TcpClient client)
+    {
+        Thread t = new Thread( () => { KeepListening(client); });
+        t.Start();
+    }
+    static void KeepListening(TcpClient client)
+    {
+        string clientIp = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+        string info = $"IP: {clientIp}";
+        Console.WriteLine($"Client connected with {info}");
+
+        try
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] bufferRead = new byte[2048];
+            byte[] bufferSend = new byte[2048];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(bufferRead, 0, bufferRead.Length)) > 0)
+            {
+                string msg = Encoding.ASCII.GetString(bufferRead, 0, bytesRead);
+                Console.WriteLine($"Receive {msg} from client: {info}");
+
+                msg = msg.ToUpper();
+
+                bufferSend = Encoding.ASCII.GetBytes(msg);
+
+                stream.Write(bufferSend, 0, bufferSend.Length);
+                Console.WriteLine($"Send {msg} to client: {info}");
+            }
+
+            Console.WriteLine("hi");
+            // stream.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            client.Close();
+            Console.WriteLine($"Client disconnected with {info}");
+        }
+    }
+}
